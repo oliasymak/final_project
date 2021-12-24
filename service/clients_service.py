f6462f5c33717abd5016e1eea2bb16dcff50ba05
oliasymak <71947423+oliasymak@users.noreply.github.com>
2021-12-24 12:01:43 +0200

    Update clients_service.py

--- service/clients_service.py
+++ service/clients_service.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import date
+from datetime import datetime
 from models.orders import OrdersList
 from models.clients import ClientsList
 
@@ -23,7 +24,8 @@ def client_add(first_name, last_name, passport_number, add_date):
     add_date (string): data from the field add date
     :return: None
     '''
-    client = ClientsList(first_name, last_name, passport_number, add_date, 1)
+    add_date1 = datetime.strptime(add_date, "%Y-%m-%d")
+    client = ClientsList(first_name, last_name, passport_number, add_date1, 1)
     db.session.add(client)
     db.session.commit()
 
@@ -41,12 +43,13 @@ def client_edit(ffirst_name, llast_name, ppassport_number, aadd_date, select_cli
     select_client (string): data from the field selected client
     :return: None
     '''
+    add_date1 = datetime.strptime(aadd_date, "%Y-%m-%d")
     client = ClientsList('Назар', 'Сохан', '888888', date(2021, 10, 30), 1)
     db.session.add(client)
     OrdersList.query.filter_by(client_passport_number=select_client).update(
         dict(client_passport_number='888888'))
     ClientsList.query.filter_by(passport_number=select_client).update(
-        dict(first_name=ffirst_name, last_name=llast_name, passport_number=ppassport_number, add_date=aadd_date))
+        dict(first_name=ffirst_name, last_name=llast_name, passport_number=ppassport_number, add_date=add_date1))
     OrdersList.query.filter_by(client_passport_number='888888').update(
         dict(client_passport_number=ppassport_number))
     ClientsList.query.filter(ClientsList.passport_number == '888888').delete()
