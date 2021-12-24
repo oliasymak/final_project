7dd3333f0160551589b9e95dd9399d4097de316e
oliasymak <71947423+oliasymak@users.noreply.github.com>
2021-12-24 13:08:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
+from models.populate_database import *
 from views.orders_view import *
 from views.clients_view import *
 from views.cars_view import *
-
 
 if __name__ == "__main__":
     app.run()
